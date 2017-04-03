6ef408dfcb2d350d97c7ce37fcde6b980b34840c
Ardi Nusawan <ardi.nusawan13@gmail.com>
2017-04-03 18:43:06 +0700

    get CPU usage

--- app/app.py
+++ app/app.py
@@ -1,9 +1,53 @@
 from flask import Flask
 import docker
 from flask import jsonify
+import logging
 
 app = Flask(__name__)
 client = docker.from_env()
+
+def get_CPU_Percentage(con):
+    conName = con.name
+    cpupercentage = 0.0
+
+    # Check if the container is running
+    if (con.status != 'running'):
+        raise ValueError('"%s" container is not running' % conName)
+
+    # Get CPU Usage in percentage
+    constat = con.stats(stream=False)
+    prestats = constat['precpu_stats']
+    cpustats = constat['cpu_stats']
+    # print(cpustats)
+
+    # cpuDelta = res.cpu_stats.cpu_usage.total_usage -  res.precpu_stats.cpu_usage.total_usage;
+    # systemDelta = res.cpu_stats.system_cpu_usage - res.precpu_stats.system_cpu_usage;
+    # var RESULT_CPU_USAGE = cpuDelta / systemDelta * 100;
+    # CPUStats.CPUUsage.PercpuUsage
+
+
+    prestats_totalusage = prestats['cpu_usage']['total_usage']
+    stats_totalusage = cpustats['cpu_usage']['total_usage']
+    numOfCPUCore = len(cpustats['cpu_usage']['percpu_usage'])
+    logging.debug('prestats_totalusage: %s, stats_totalusage: %s, NoOfCore: %s' % (
+    prestats_totalusage, stats_totalusage, numOfCPUCore))
+
+    prestats_syscpu = prestats['system_cpu_usage']
+    stats_syscpu = cpustats['system_cpu_usage']
+    logging.debug('prestats_syscpu: %s, stats_syscpu: %s' % (prestats_syscpu, stats_syscpu))
+
+    cpuDelta = stats_totalusage - prestats_totalusage
+    systemDelta = stats_syscpu - prestats_syscpu
+
+    if cpuDelta > 0 and systemDelta > 0:
+        cpupercentage = (cpuDelta / systemDelta) * numOfCPUCore
+
+    formattedcpupert = '{:.1%}'.format(cpupercentage)
+    logging.debug('cpuDelta: %s, systemDelta: %s, cpu: %s' % (cpuDelta, systemDelta, cpupercentage))
+
+    logging.info('"%s" Container CPU: %s ' % (conName, formattedcpupert))
+
+    return (cpupercentage * 100)
 
 @app.route("/")
 def hello():
@@ -13,8 +57,11 @@ def hello():
 def container_list():
     list = client.containers.list()
     print(dir(list[0]))
-    print(list[0].attrs)
-    return jsonify(list[0].attrs)
+    con = client.containers.get("c81912cb33fb")
+
+    con_perc = get_CPU_Percentage(con)
+    # con_stats = con.stats(stream=False)
+    return jsonify(con_perc)
 
 if __name__ == "__main__":
     # app.debug = True
