3d2781d593f3234acf1401b5972ad076cd9474f4
Ardi Nusawan <ardi.nusawan13@gmail.com>
2017-04-09 16:30:39 +0700

    adding pseudo ahp and database sqlite

---
+++ app/ahp.py
@@ -0,0 +1,24 @@
+import numpy as np
+import math
+
+
+def f(num):
+    return math.sqrt(math.sqrt(num))
+
+
+data = np.matrix([
+    [1, 1 / 3, 1 / 9, 1 / 5],
+    [3, 1, 1, 1],
+    [9, 1, 1, 3],
+    [5, 1, 1 / 3, 1]])
+sum = 0
+t = []
+for idx, val in enumerate(data):
+    temp = 1
+    for val2 in range(val.size):
+        # print(val.item(val2))
+        temp = temp * val.item(val2)
+    t.append(f(temp))
+    print(t[idx])
+    sum += f(temp)
+print(t[0] / sum)
