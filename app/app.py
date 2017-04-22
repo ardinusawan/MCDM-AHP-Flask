701e424cbf25efe321e9d007f2ca6f8f240f50fa
Ardi Nusawan <ardi.nusawan13@gmail.com>
2017-04-22 16:32:20 +0700

    get last data
    Signed-off-by: Ardi Nusawan <ardi.nusawan13@gmail.com>

--- app/app.py
+++ app/app.py
@@ -1,44 +1,49 @@
-from flask import Flask
-from flask import request
-from flask import jsonify
 import atexit
+
+import utils as utils
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
-import utils as UTILS
-import ahp as AHP
-import db as DB
+from flask import Flask
+from flask import jsonify
+from flask import request
+
 app = Flask(__name__)
 
-get_stats = 10 # minutes
+get_stats = 10  # minutes
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
+
 @app.route("/container/list")
 def container_list():
-    UTILS.stats()
+    utils.stats()
     # print(dir(list[0]))
-    con_log = ''
-    return (con_log)
+    con_log = ""
+    utils.stats()
+    return con_log
+
 
 @app.route("/AHP/1", methods=['GET', 'POST'])
 def computing_vector():
     if request.method == 'POST':
         data = request.get_json()
-        status = DB.insert_comparisonMatric(data['parameter_data'],**data['comparison'])
+        # status = database.insert_comparison_matrix(data['parameter_data'], **data['comparison'])
         return jsonify(data)
     else:
-        return jsonify({"Message":"Success"},sort_keys = False, indent = 2)
+        return jsonify({"Message": "Success"}, sort_keys=False, indent=2)
 
-#schedule to write stats to DB
+
+# schedule to write stats to DB
 scheduler = BackgroundScheduler()
 scheduler.start()
 scheduler.add_job(
-    func=UTILS.stats,
+    func=utils.stats,
     trigger=IntervalTrigger(minutes=get_stats),
     id='get_docker_stats',
-    name='Get Docker Stats every ten minutes',
+    name='Get Docker Stats every %s minutes' % get_stats,
     replace_existing=True)
 # Shut down the scheduler when exiting the app
 atexit.register(lambda: scheduler.shutdown())
