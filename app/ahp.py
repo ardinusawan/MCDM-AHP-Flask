55aabc7ff94423af2c2e89f6ab28c37051e7eb5f
Ardi Nusawan <ardi.nusawan13@gmail.com>
2017-04-25 17:31:41 +0700

    first step done
    Signed-off-by: Ardi Nusawan <ardi.nusawan13@gmail.com>

--- app/ahp.py
+++ app/ahp.py
@@ -2,9 +2,9 @@ import numpy as np
 import math
 import db as database
 import pprint
-
+import pandas as pd
 from flask import jsonify
-
+import re
 pp = pprint.PrettyPrinter()
 
 
@@ -12,171 +12,73 @@ def f(num):
     return math.sqrt(math.sqrt(num))
 
 
-def get_container(**kwargs):
-    kwargs["sort"] = {"column": "name", "order": "ASD"}
-    kwargs["select"] = {"data": "container_id"}
+def weight_of_criteria(*args,**kwargs):
+    Memory_raw = {"Memory":{0:1,1:2, 2:4}}
+    LTA_raw = {"LTA": {0:0.5, 1:1, 2:2}}
+    CPU_raw = {"CPU":{0:0.25,1:0.5, 2:1}}
+    Memory = pd.DataFrame(Memory_raw)
+    LTA = pd.DataFrame(LTA_raw)
+    CPU = pd.DataFrame(CPU_raw)
+    result = Memory.join(LTA)
+    result = result.join(CPU)
+    return result
 
-    data = database.all_data("containers", **kwargs)
-    return data
+def each_node(*args,**kwargs):
+    # langkah2
+    # 1. mendapatkan semua data CPU, memory, LTA, cari terendah dan tertinggi, bagi 9 kolom
+    kwargs["column"] = "container_id, timestamps"
+    kwargs["where"] = "status = 'running'"
+    container = database.select("containers",**kwargs)
+    c_id = '{}'.format([x[0] for x in container])
+    c_id = "".join(c_id)
+    c_id = c_id[1:-1]
+    ts = container[0][1]
 
+    kwargs.clear()
+    kwargs["column"] = "container_id, cpu"
+    kwargs["where"] = "container_id IN ({c_id}) AND timestamps = '{ts}'".format(c_id=c_id,ts=ts)
+    cpu = database.select("stats",**kwargs)
+    cpu_min = min(cpu, key=lambda key: key[1])
+    cpu_max = max(cpu, key=lambda key: key[1])
+    cpu_range = list(np.arange(cpu_min[1], cpu_max[1], (cpu_max[1]-cpu_min[1])/9))
 
-def status():
-    if database.total_data("containers") == 0:
-        return False
-    else:
-        return True
+    kwargs.clear()
+    kwargs["column"] = "container_id, memory"
+    kwargs["where"] = "container_id IN ({c_id}) AND timestamps = '{ts}'".format(c_id=c_id, ts=ts)
+    memory = database.select("stats",**kwargs)
+    memory_min = min(memory, key=lambda key: key[1])
+    memory_max = max(memory, key=lambda key: key[1])
+    memory_range = list(np.arange(memory_min[1], memory_max[1], (memory_max[1] - memory_min[1]) / 9))
 
-# langkah 1
-# data = np.array(
-#     [
-#         (1.0, 1.0 / 3, 1 / 9, 1 / 5),
-#         (3.0, 1.0, 1.0, 1.0),
-#         (9.0, 1.0, 1.0, 3.0),
-#         (5.0, 1.0, 1 / 3, 1.0)],dtype=[('MSFT','float'),('CSCO','float'),('GOOG','float'),('F','float')])
-def weight_of_criteria():
-    data = np.matrix([
-        # memory,waktu,cpu
-        [1, 2, 4],
-        [0.5, 1, 2],
-        [0.25, 0.5, 1]])
-    total = 0
-    weigh_of_criteria = []
-    for idx, val in enumerate(data):
-        temp = 1
-        for val2 in range(val.size):
-            temp = temp * val.item(val2)
-        weigh_of_criteria.append(f(temp))
-        # print(t[idx])
-        total += f(temp)
+    kwargs.clear()
+    kwargs["column"] = "container_id, last_time_access_percentage"
+    kwargs["where"] = "container_id IN ({c_id}) AND timestamps = '{ts}'".format(c_id=c_id, ts=ts)
+    lta = database.select("stats", **kwargs)
+    lta_min = min(lta, key=lambda key: key[1])
+    lta_max = max(lta, key=lambda key: key[1])
+    lta_range = list(np.arange(lta_min[1], lta_max[1], (lta_max[1] - lta_min[1]) / 9))
 
-    for i in range(len(weigh_of_criteria)):
-        weigh_of_criteria[i] = weigh_of_criteria[i] / total
-    # memory,waktu,cpu
-    return weigh_of_criteria
-
-
-'''
-def rating_of_each_node(*args, **kwargs):
-    params = ''.join(args)
-    if params == "LTA":
-        params = 7
-    table_name = "containers"
-    sort = {"column": "name", "order": "ASD"}
-    total = database.total_data(table_name)
-
-    # mendapatkan semua container id, ASC nama kontainer
-    data = database.all_data(table_name, **sort)
-
-    temp = np.zeros((total,total))
-    for i in range(total):
-        # mendapatkan data terakhir pada stats
-        ts = "'" + data[i][3].strftime('%Y-%m-%d %H:%M:%S') + "'"
-        container_id = "'" + data[i][0] + "'"
-        find_by = {"container_id": container_id, "timestamps": ts}
-        res1 = database.find_data("stats", **find_by)
-        # hitung dari 1 - n, n1/nN
-        for j in range(total):
-            # mendapatkan data terakhir pada stats
-            ts = "'" + data[j][3].strftime('%Y-%m-%d %H:%M:%S') + "'"
-            container_id = "'" + data[j][0] + "'"
-            find_by = {"container_id": container_id, "timestamps": ts}
-            res2 = database.find_data("stats", **find_by)
-            # print(res[0][params])
-            temp[i][j] = res1[0][params]/res2[0][params]
+    # 2. untuk setiap data, cek data ada pada kolom mana, masukkan ke matrik
+    # 3. kalikan row dan column
+    # 4. untuk setiap row, cari nilai geomean
+    # 5. untuk setiap row, cari eigenvector
+    # 6. pastikan nilai sum of eigenvector = 1
 
 
-        # temp = data[i]
-        # setelah dapat, buat skala antara 1 - 9, dari 0 sampai +- data terbesar
-        # untuk setiap hasil perbandingan, bandingkan cocoknya berada pada posisi mana, apakah - atau 0 atau +
-        # taruh dalam list
-    print(temp)
-    print("min: ",np.amin(temp),"\nmax: ",np.amax(temp))
-'''
+    # membandingkan
+    # for i in range(len(result.columns)):
+    #     for j in range(len(result.columns)):
+    #         # result.iloc[i][j] = result.iloc[i][j] / result.iloc[j][i]
+    #         # result.iloc[j][i] = 1 / result.iloc[i][j]
+    #         pass
+    # geomean dan eigenvector
+    # for i in range(len(result.columns)):
+    #     for j in range(len(result.columns)):
+    #         pass
+            # result.index.names = "A"
+            # # result.index = {"docker1","docker2"}
+            # a = result.iloc[1][0] * result["docker2"][1]
+            # print(result)
+            # print(a)
 
-
-def rating_of_each_node(*args, **kwargs):
-    if not status():
-        return False
-    params = ''.join(args)
-    if params == "CPU":
-        params = 3
-    elif params == "Memory":
-        params = 5
-    elif params == "LTA":
-        params = 7
-    table_name = "containers"
-    timestamps_position = 3
-    total = database.total_data(table_name)
-    kwargs["sort"] = {"column": "name", "order": "ASD"}
-    data = database.all_data(table_name, **kwargs)
-
-    temp = np.zeros(total)
-    sum = 0
-    for i in range(0,total):
-        # mendapatkan data terakhir pada stats
-        ts = '{}'.format(data[i][timestamps_position].strftime('%Y-%m-%d %H:%M:%S'))
-        container_id = '{}'.format(data[i][0])
-        kwargs["where"] = "container_id = '{container_id}' AND timestamps = '{timestamps}'".format(container_id=container_id,timestamps=ts)
-        # find_by = {"container_id": container_id, "timestamps": ts}
-        res = database.select("stats", **kwargs)
-        if len(res) == 0:
-            # print(jsonify({"status":"error"},{"message":"Not found data in stats"}))
-            break
-        else:
-            sum += res[0][params]
-
-    for j in range(0,total):
-        ts = '{}'.format(data[j][timestamps_position].strftime('%Y-%m-%d %H:%M:%S'))
-        container_id = '{}'.format(data[j][0])
-        kwargs["where"] = "container_id = '{container_id}' AND timestamps = '{timestamps}'".format(container_id=container_id,timestamps=ts)
-        res = database.select("stats", **kwargs)
-        if len(res) == 0:
-            # print(jsonify({"status":"error"},{"message":"Not found data in stats"}))
-            break
-        if sum == 0:
-            temp[j] = 0
-        else:
-            temp[j] = res[0][params] / sum
-    return temp
-
-
-def final_score():
-    if not status():
-        return False
-    containers_total = database.total_data("containers")
-    parameter_total = database.total_data("parameter")
-
-    option = weight_of_criteria()
-
-    score = [0] * containers_total
-    node = [0] * parameter_total
-    calculate = "Memory"
-    node[0] = rating_of_each_node(*calculate)
-    calculate = "LTA"
-    node[1] = rating_of_each_node(*calculate)
-    calculate = "CPU"
-    node[2] = rating_of_each_node(*calculate)
-
-    for i in range(0, containers_total):
-        for j in range(0, parameter_total):
-            score[i] += node[j][i] * option[j]
-
-    container = []
-    for item in get_container():
-        container.append(str(item[0]))
-
-    result = dict(zip(container, score))
-    container_selected = max(result, key=lambda key: result[key])
-    # print("Last Score for each node:")
-    # pp.pprint(data)
-    # print("Best container to kill:",container_selected)
-    text = "selected"
-    params = ''.join(text)
-    container_selected = {params: container_selected}
-
-    text = "result"
-    params = ''.join(text)
-    result = {params: result}
-
-    msg = {**container_selected, **result}
-    return msg
+each_node()
