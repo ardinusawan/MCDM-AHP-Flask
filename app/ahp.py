3d41236f2d82d6ef749a129753e0d77fbd8e3de8
Ardi Nusawan <ardi.nusawan13@gmail.com>
2017-04-26 04:24:23 +0700

    cpu done
    Signed-off-by: Ardi Nusawan <ardi.nusawan13@gmail.com>

--- app/ahp.py
+++ app/ahp.py
@@ -26,8 +26,9 @@ def weight_of_criteria(*args,**kwargs):
 def each_node(*args,**kwargs):
     # langkah2
     # 1. mendapatkan semua data CPU, memory, LTA, cari terendah dan tertinggi, bagi 9 kolom
-    kwargs["column"] = "container_id, timestamps"
+    kwargs["column"] = "container_id, timestamps, name"
     kwargs["where"] = "status = 'running'"
+    kwargs["sort"] = "name"
     container = database.select("containers",**kwargs)
     c_id = '{}'.format([x[0] for x in container])
     c_id = "".join(c_id)
@@ -35,8 +36,9 @@ def each_node(*args,**kwargs):
     ts = container[0][1]
 
     kwargs.clear()
-    kwargs["column"] = "container_id, cpu"
+    kwargs["column"] = "container_id, cpu, container_name"
     kwargs["where"] = "container_id IN ({c_id}) AND timestamps = '{ts}'".format(c_id=c_id,ts=ts)
+    kwargs["sort"] = "container_name"
     cpu = database.select("stats",**kwargs)
     cpu_min = min(cpu, key=lambda key: key[1])
     cpu_max = max(cpu, key=lambda key: key[1])
@@ -45,6 +47,7 @@ def each_node(*args,**kwargs):
     kwargs.clear()
     kwargs["column"] = "container_id, memory"
     kwargs["where"] = "container_id IN ({c_id}) AND timestamps = '{ts}'".format(c_id=c_id, ts=ts)
+    kwargs["sort"] = "container_name"
     memory = database.select("stats",**kwargs)
     memory_min = min(memory, key=lambda key: key[1])
     memory_max = max(memory, key=lambda key: key[1])
@@ -53,32 +56,52 @@ def each_node(*args,**kwargs):
     kwargs.clear()
     kwargs["column"] = "container_id, last_time_access_percentage"
     kwargs["where"] = "container_id IN ({c_id}) AND timestamps = '{ts}'".format(c_id=c_id, ts=ts)
+    kwargs["sort"] = "container_name"
     lta = database.select("stats", **kwargs)
     lta_min = min(lta, key=lambda key: key[1])
     lta_max = max(lta, key=lambda key: key[1])
     lta_range = list(np.arange(lta_min[1], lta_max[1], (lta_max[1] - lta_min[1]) / 9))
 
     # 2. untuk setiap data, cek data ada pada kolom mana, masukkan ke matrik
-    # 3. kalikan row dan column
-    # 4. untuk setiap row, cari nilai geomean
-    # 5. untuk setiap row, cari eigenvector
+    c_name = [x[2] for x in container]
+    index = c_name
+    columns = c_name
+    type_data = ['float32'] * len(c_name)
+    dtype = list(zip(c_name,type_data))
+    values = np.zeros(len(c_name), dtype = dtype)
+    cpu_matrix = pd.DataFrame(values, index=index, columns=columns)
+    for row in enumerate(cpu_matrix.index.values):
+        for column in enumerate(cpu_matrix.columns.values):
+            tx = [item for item in cpu if item[2] == row[1]][0][1]
+            ty = [item for item in cpu if item[2] == column[1]][0][1]
+
+            for idx,val in enumerate(cpu_range[1:len(cpu_range)]):
+                if (tx >=cpu_range[idx-1] and tx < cpu_range[idx]):
+                    tx = idx
+                    break
+                elif tx>=cpu_range[len(cpu_range)-1]:
+                    tx = len(cpu_range)
+                    break
+
+            for idx,val in enumerate(cpu_range[1:len(cpu_range)]):
+                if (ty >=cpu_range[idx-1] and ty < cpu_range[idx]):
+                    ty = idx
+                    break
+                elif ty>=cpu_range[len(cpu_range)-1]:
+                    ty = len(cpu_range)
+                    break
+
+            # 3. Bandingkan row dan column
+            tz = tx/ty
+            cpu_matrix.iloc[cpu_matrix.index.get_loc(row[1]), cpu_matrix.columns.get_loc(column[1])] = tz
+            # cpu_matrix = cpu_matrix.where(np.triu(np.ones(cpu_matrix.shape)))
+            # cpu_matrix = cpu_matrix.transpose()
+
+    # 4. untuk setiap row, cari nilai geomean dan eigenvector
+    cpu_matrix["3rd root of product"] = cpu_matrix.product(axis=1) ** (1 / 3)
+    cpu_matrix["priority vector"] = cpu_matrix["3rd root of product"] / cpu_matrix["3rd root of product"].sum()
+    print(cpu_matrix)
+
     # 6. pastikan nilai sum of eigenvector = 1
-
-
-    # membandingkan
-    # for i in range(len(result.columns)):
-    #     for j in range(len(result.columns)):
-    #         # result.iloc[i][j] = result.iloc[i][j] / result.iloc[j][i]
-    #         # result.iloc[j][i] = 1 / result.iloc[i][j]
-    #         pass
-    # geomean dan eigenvector
-    # for i in range(len(result.columns)):
-    #     for j in range(len(result.columns)):
-    #         pass
-            # result.index.names = "A"
-            # # result.index = {"docker1","docker2"}
-            # a = result.iloc[1][0] * result["docker2"][1]
-            # print(result)
-            # print(a)
 
 each_node()
