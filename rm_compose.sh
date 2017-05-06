37aa76a3d365047f10c107408c07256066fe42f1
Ardi Nusawan <ardi.nusawan13@gmail.com>
2017-05-06 11:31:23 +0700

    adding new syntax
    Signed-off-by: Ardi Nusawan <ardi.nusawan13@gmail.com>

--- rm_compose.sh
+++ rm_compose.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 for i in $(seq 1 10);
     do
+        cd ./moodle/$i/
         nohup docker rm moodle$i mariadb$i &
+        rm -R apache_data/ mariadb_data/ moodle_data/ php_data/
+        cd -
     done
