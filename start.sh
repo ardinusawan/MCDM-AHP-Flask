4889e214a96b0c9dc360bacc4e61c826cf4750ab
ardinusawan <ardi.nusawan13@gmail.com>
2017-06-04 20:19:34 +0700

    ready to test

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 source $PWD/venv/bin/activate
-cd app
-uwsgi --socket 0.0.0.0:5000 --protocol=http -w wsgi
+python3 $PWD/app/app.py& PID=$!; sleep 7200; kill $PID
+#cd app
+#uwsgi --socket 0.0.0.0:5000 --protocol=http -w wsgi
