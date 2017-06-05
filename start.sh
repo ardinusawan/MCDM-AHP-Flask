72b012388de0481d092096f4314792cd71217bb7
ardinusawan <ardi.nusawan13@gmail.com>
2017-06-05 12:12:14 +0700

    fix test

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 source $PWD/venv/bin/activate
-python3 $PWD/app/app.py& PID=$!; sleep 7200; kill $PID
+python3 -u $PWD/app/app.py& PID=$!; sleep 3600; kill $PID
 #cd app
 #uwsgi --socket 0.0.0.0:5000 --protocol=http -w wsgi
