ff548810d0de68c9f74a1c5704752588962aa896
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2017-06-14 19:20:47 +0700

    update localhost to pc lp ip

--- app/app.py
+++ app/app.py
@@ -30,7 +30,7 @@ def moodle(moodle_id):
         moodle_id = str(10010)
     else:
         moodle_id = str(1000) + moodle_id
-    url = "http://localhost:" + moodle_id
+    url = "http://10.151.34.236:" + moodle_id
     return redirect(url, code=302)
 
 @application.route("/compute")
